65874d52115536c867644d51b482cae2d46f0d36
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 16:40:25 +0530

    Update Q9.js

--- Q9.js
+++ Q9.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Get all where dept is IT
 connection.employeesequelize.findAll({where:{dept:'IT'},raw:true})
 .then(data=>{
     console.log(data);
@@ -7,7 +8,7 @@ connection.employeesequelize.findAll({where:{dept:'IT'},raw:true})
 .catch(err=>{
     console.log(err);
 })
-
+//Get all where dept is IT with query
 connection.sequelize.query("select * from `EmployeeTable` where dept='IT'",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);

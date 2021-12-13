752723c7a18bdcae69db7039467d34e6d50acff9
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:33:22 +0530

    Update Q10.js

--- Q10.js
+++ Q10.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Count All Employee
 connection.employeesequelize.findAndCountAll({raw:true})
 .then(data=>{
     console.log("Total Count Of Employee : ",data.count);
@@ -8,6 +9,7 @@ connection.employeesequelize.findAndCountAll({raw:true})
     console.log(err);
 })
 
+//Executing With query
 connection.sequelize.query("select count(*) from `EmployeeTable`",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);

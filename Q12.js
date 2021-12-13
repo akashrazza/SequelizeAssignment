94d941ca7d963adcecd493bcb4314b3ca59be509
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:35:47 +0530

    Update Q12.js

--- Q12.js
+++ Q12.js
@@ -1,7 +1,9 @@
 var connection = require('./connection');
 
+//Instance of Sequelize Operation
 op=connection.Sequelize.Op
 
+//Get Employee using like operator
 connection.employeesequelize.findAll({where:{
     name:{
     [op.like]:'%a%'
@@ -14,6 +16,7 @@ connection.employeesequelize.findAll({where:{
     console.log(err);
 })
 
+//Executing With query
 connection.sequelize.query("select * from `EmployeeTable` where name like '%a%'",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);

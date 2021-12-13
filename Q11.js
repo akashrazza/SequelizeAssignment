fbf868fac406fdc2a5dfa86b1e5781c9cdbdebf2
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:34:24 +0530

    Update Q11.js

--- Q11.js
+++ Q11.js
@@ -1,9 +1,11 @@
 var connection = require('./connection');
 
+//Get All employee in asscending order
 connection.employeesequelize.findAll({order:[['name','ASC']],raw:true})
 .then(data=>{console.log(data)})
 .catch(err=>{console.log(err)})
 
+//Execute With query
 connection.sequelize.query("select * from `EmployeeTable` order by name asc ",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);

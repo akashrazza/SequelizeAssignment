a4851052833f68a2f77fc03a0223b46f31dfccc7
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:56:49 +0530

    Update Q20.js

--- Q20.js
+++ Q20.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Delete Table
 connection.employeesequelize.drop()
 .then(()=>{console.log("table deleted sucessfully");})
 .catch(err=>{console.log(err)})

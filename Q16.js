29efe93a1a2cfbb18bbfd36a6e51a03b5ba29eac
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:51:37 +0530

    Update Q16.js

--- Q16.js
+++ Q16.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Insert into Employee Sequelize
 var data = connection.employeesequelize.build(
     {EmpId:11,name:'test3',dept:'test3',designation:'test3'}
 )

6012e2d761416c92f81d05bb2b9668544735a133
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 15:52:24 +0530

    Update Q17.js

--- Q17.js
+++ Q17.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Bulk Insert into Employee Sequelize
 connection.employeesequelize.bulkCreate([
     {EmpId:1,name:'Raja',dept:'FullStack',designation:'trainee'},
     {EmpId:2,name:'Gyan',dept:'FullStack',designation:'trainee'},

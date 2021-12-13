09ed9ae6def9b02bc6c62bb86da9855677e0d9d3
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 16:38:14 +0530

    Update Q7.js

--- Q7.js
+++ Q7.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//Get All From EMployee table
 connection.employeesequelize.findAll({where:{name:'Raja'},raw:true})
 .then(data=>{
     console.log(data);
@@ -8,6 +9,7 @@ connection.employeesequelize.findAll({where:{name:'Raja'},raw:true})
     console.log(err);
 })
 
+//Get Employee table with name
 connection.sequelize.query("select * from `EmployeeTable` where name='Raja'",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);

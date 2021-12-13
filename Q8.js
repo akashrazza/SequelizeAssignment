f332fc8fdc537d81e8100a700ccb1841b748d120
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 16:39:21 +0530

    Update Q8.js

--- Q8.js
+++ Q8.js
@@ -1,5 +1,6 @@
 var connection = require('./connection');
 
+//get all records shows only name and dept
 connection.employeesequelize.findAll({attributes:['name','dept'],raw:true})
 .then(data=>[
     console.log(data)
@@ -8,6 +9,7 @@ connection.employeesequelize.findAll({attributes:['name','dept'],raw:true})
     console.log(err)
 })
 
+//get all records shows only name and dept with query
 connection.sequelize.query("select name,dept from `EmployeeTable`",{type:connection.Sequelize.QueryTypes.SELECT})
 .then(data=>{
     console.log(data);
